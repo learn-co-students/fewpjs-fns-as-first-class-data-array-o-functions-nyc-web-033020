3c7179300872ec826759b2df865e85dd8e9cae5c
Edgar <edgar.a.rivera08@gmail.com>
2020-05-11 22:19:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function wakeDog(dogName='Titan', dogBreed='Pit') {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+}
+
+function leashDog(dogName='Titan', dogBreed='Pit') {
+    console.log(`Leash ${dogName} the ${dogBreed}`);   
+    return `Leash ${dogName} the ${dogBreed}`
+}
+
+function walkToPark(dogName='Titan', dogBreed='Pit') {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName='Titan', dogBreed='Pit') {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
+
+function walkHome(dogName='Titan', dogBreed='Pit') {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`)
+    return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+function unleashDog(dogName='Titan', dogBreed='Pit') {
+    console.log(`Unleash ${dogName} the ${dogBreed}`)
+    return `Unleash ${dogName} the ${dogBreed}`
+}
+
+const routine = [
+    wakeDog,
+    leashDog,
+    walkToPark,
+    throwFrisbee,
+    walkHome,
+    unleashDog
+]
+
+function exerciseDog(dogName, dogBreed) {
+   return routine.map(fn => fn(dogName, dogBreed))
+}
